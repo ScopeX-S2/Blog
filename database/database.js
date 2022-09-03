338aadd7f07e5f933ce753d265213a8186cc5b1f
SX7 SCRIPT <85971697+ScopeX-S2@users.noreply.github.com>
2022-09-03 13:32:20 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const connection = new Sequelize('blog', 'root', '44334455', {
+const connection = new Sequelize('blog', 'root', 'sua-senha-vai-aqui', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
